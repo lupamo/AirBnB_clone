32f5a3bf2b164b98b73c00d8f1385d758b110483
lupamo <arnoldlupamo@gmail.com>
2024-02-17 22:32:39 +0300

    kwarg and args

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,19 @@ import models
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initalizing instances """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at.now()
+
+        if len(kwargs) != 0:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = val
+
 
     def __str__(self):
         """returns a string representation of an object"""
