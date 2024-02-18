f5e583adb229749ba7625019682d0a56cd7edd65
lolosaisa <lwamaitha3924@gmail.com>
2024-02-18 11:48:33 -0800

    classes hat inherit from the basemodel

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module creates a review class"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Class for managing review projects"""
+    
+    place_id = ""
+    user_id = ""
+    text = ""
